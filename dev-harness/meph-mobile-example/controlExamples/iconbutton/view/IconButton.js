a530dce97a47b5723b9c6cbd61e32d7011462b97
Andrew Porter <mephisto83@hotmail.com>
2014-07-03 12:45:22 +0200

    Trying to check in

--- dev-harness/meph-mobile-example/controlExamples/iconbutton/view/IconButton.js
+++ dev-harness/meph-mobile-example/controlExamples/iconbutton/view/IconButton.js
@@ -1,7 +1,7 @@
 ﻿MEPH.define('MEPHControls.iconbutton.view.IconButton', {
     alias: 'mephcontrols_iconbutton',
     templates: true,
-    extend: 'MEPH.control.Control',
+    extend: 'MEPH.mobile.activity.container.Container',
     mixins: ['MEPH.mobile.mixins.Activity'],
     requires: ['MEPH.mobile.activity.view.ActivityView', 'MEPH.button.IconButton'],
     properties: {
@@ -11,32 +11,9 @@
     initialize: function () {
         var me = this;
         me.callParent.apply(me, arguments);
-        //me.on('load', me.onLoaded.bind(me));
     },
     onLoaded: function () {
         var me = this;
         me.name = 'Icon Buttons';
-    },
-    show: function () {
-        var me = this,
-            view,
-            dom;
-        dom = me.getDomTemplate();
-        view = dom.first();
-        return me.viewTransition(view, { remove: me.$removeHomePageCls });
-    },
-    hide: function () {
-        var me = this,
-            view,
-            dom = me.getDomTemplate();
-
-        view = dom.first();
-        return me.viewTransition(view, { add: me.$removeHomePageCls });
-    },
-    close: function () {
-        var me = this;
-    },
-    open: function () {
-        var me = this;
     }
 });
