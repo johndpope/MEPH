d7a85640c5fadafa6be669f8e61e2716a2e6566b
Andrew Porter <mephisto83@hotmail.com>
2014-08-30 20:40:50 +0200

    calculate primes
    Added a function for calculating prime numbers, and implemented the
    caching of the primes.

--- meph/specs/math/Util.spec.js
+++ meph/specs/math/Util.spec.js
@@ -14,4 +14,32 @@
         });
     });
 
+    it('a selection of prime numbers can be computed up to a val', function (done) {
+        MEPH.requires('MEPH.math.Util').then(function () {
+            
+            var primes = MEPH.math.Util.primes(10);
+            
+            expect(primes.length === 4).theTruth('the number of primes was incorrect.');
+        }).catch(function (e) {
+            expect(new Error(e)).caught();
+        }).then(function (x) {
+            done();
+        });
+    });
+
+
+
+    it('a selection of prime numbers can be computed up to a val', function (done) {
+        MEPH.requires('MEPH.math.Util').then(function () {
+            
+            var primes = MEPH.math.Util.primes(100);
+
+            expect(primes.length === 25).theTruth('the number of primes was incorrect.');
+        }).catch(function (e) {
+            expect(new Error(e)).caught();
+        }).then(function (x) {
+            done();
+        });
+    });
+
 });
--- meph/src/math/Util.js
+++ meph/src/math/Util.js
@@ -5,12 +5,36 @@
  **/
 MEPH.define('MEPH.math.Util', {
     statics: {
+        cachedPrimes: null,
+        /**
+         * n mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n.
+         * http://en.wikipedia.org/wiki/Factorial
+         * Calculates the factorial of num.
+         **/
         factorial: function (num) {
             var result = 1;
             [].interpolate(1, num + 1, function (x) {
                 result = result * x;
             });
             return result;
+        },
+        /**
+         * Returns the primes up to the passed value.
+         * @param {Number} val
+         **/
+        primes: function (val) {
+            MEPH.math.Util.cachedPrimes = MEPH.math.Util.cachedPrimes || [2, 3];
+            var cachedPrimes = MEPH.math.Util.cachedPrimes;
+            var last = MEPH.math.Util.cachedPrimes.last();
+            if (last >= val) {
+                return MEPH.math.Util.cachedPrimes.where(function (x) { return val >= x; });
+            }
+            for (var i = last + 2; i <= (val) ; i = i + 2) {
+                if (cachedPrimes.all(function (x) { return i % x !== 0; })) {
+                    cachedPrimes.push(i);
+                }
+            }
+            return cachedPrimes;
         }
     }
 });
