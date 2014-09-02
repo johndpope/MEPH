d5977059929c6b26ed3e6e26ad771d634477a6ed
Andrew Porter <mephisto83@hotmail.com>
2014-09-02 21:14:36 +0200

    more logic for factorization
    added mathjax 2.3 to externals , and more logic for factorization. Power
    functions can be collapsed to their smallest representations. Created a
    fluent intersection function for arrays.

---
+++ meph/specs/math/expression/Factor.spec.js
@@ -0,0 +1,95 @@
+﻿describe("MEPH/math/expression/Factor.spec.js", function () {
+    beforeEach(function () {
+        jasmine.addMatchers(MEPH.customMatchers);
+    });
+
+    it('an expression can be created', function (done) {
+
+        MEPH.requires('MEPH.math.expression.Factor').then(function ($class) {
+
+            var factor = new MEPH.math.expression.Factor();
+
+            expect(factor).theTruth('a factor was not created');
+
+        }).catch(function (e) {
+            expect(e).caught();
+        }).then(function (x) {
+            done();
+        });
+    });
+
+
+    it('can get factors of a "variable" ', function (done) {
+
+        MEPH.requires('MEPH.math.Expression', 'MEPH.math.expression.Factor').then(function ($class) {
+
+            var variable = Expression.variable('x');
+            var factors = MEPH.math.expression.Factor.getFactors(variable);
+            expect(factors).toBeTruthy();
+            expect(factors.length === 1).toBeTruthy();
+            expect(factors.first().exp.equals(Expression.variable('x'))).toBeTruthy();
+            expect(factors.first().count === 1).toBeTruthy();
+
+        }).catch(function (e) {
+            expect(e).caught();
+        }).then(function (x) {
+            done();
+        });
+    });
+
+
+    it('can get factors of a "power" x^2 ', function (done) {
+
+        MEPH.requires('MEPH.math.Expression', 'MEPH.math.expression.Factor').then(function ($class) {
+
+            var power = Expression.power(Expression.variable('x'), Expression.variable('2'));
+            var factors = MEPH.math.expression.Factor.getFactors(power);
+            expect(factors).toBeTruthy();
+            expect(factors.length === 1).toBeTruthy();
+            expect(factors.first().exp.type === Expression.type.variable).toBeTruthy();
+            expect(factors.first().count === 2).toBeTruthy();
+
+        }).catch(function (e) {
+            expect(e).caught();
+        }).then(function (x) {
+            done();
+        });
+    });
+
+    it('can get factors of a "power" (x^2)^2 ', function (done) {
+
+        MEPH.requires('MEPH.math.Expression', 'MEPH.math.expression.Factor').then(function ($class) {
+
+            var power = Expression.power(Expression.power(Expression.variable('x'), Expression.variable('2')), Expression.variable('2'));
+            var factors = MEPH.math.expression.Factor.getFactors(power);
+            expect(factors).toBeTruthy();
+            expect(factors.length === 1).toBeTruthy();
+            expect(factors.first().exp.type === Expression.type.variable).toBeTruthy();
+            expect(factors.first().count === 4).toBeTruthy();
+
+        }).catch(function (e) {
+            expect(e).caught();
+        }).then(function (x) {
+            done();
+        });
+    });
+
+    it('can get factors of a "power" x^a ', function (done) {
+
+        MEPH.requires('MEPH.math.Expression', 'MEPH.math.expression.Factor').then(function ($class) {
+
+            var power = Expression.power(Expression.variable('x'), Expression.variable('a'));
+            var factors = MEPH.math.expression.Factor.getFactors(power);
+            expect(factors).toBeTruthy();
+            expect(factors.length === 1).toBeTruthy();
+            expect(factors.first().exp.type === Expression.type.variable).toBeTruthy();
+            expect(factors.first().count === 'a').toBeTruthy();
+
+        }).catch(function (e) {
+            expect(e).caught();
+        }).then(function (x) {
+            done();
+        });
+    });
+
+});
---
+++ meph/src/math/expression/Factor.js
@@ -0,0 +1,77 @@
+﻿/**
+ * @class MEPH.math.Expression
+ * Describes mathematical expressions.
+ *
+ **/
+MEPH.define('MEPH.math.expression.Factor', {
+    statics: {
+        /**
+         * Gets factors from the expression.
+         * @param {MEPH.math.Expression} expression
+         * @return {Array}
+         **/
+        getFactors: function (expression) {
+            var Factor = MEPH.math.expression.Factor;
+            switch (expression.type) {
+                case Expression.type.variable:
+                    return Factor.getVariableFactors(expression);
+                case Expression.type.power:
+                    return Factor.getPowerFactors(expression);
+            }
+        },
+        /**
+         * Gets variable factors.
+         * @param {MEPH.math.Expression} expression
+         * @return {Array}
+         **/
+        getVariableFactors: function (expression) {
+            var Factor = MEPH.math.expression.Factor;
+            var factor = new Factor(expression, 1);
+            return [factor];
+        },
+        /**
+         * Gets power factors.
+         * @param {MEPH.math.Expression} expression
+         * @return {Array}
+         **/
+        getPowerFactors: function (expression) {
+            var Factor = MEPH.math.expression.Factor;
+            var copy = expression.copy();
+            var flattenedCopy = Expression.FlattenPower(copy);
+            var power = flattenedCopy.partOrDefault(Expression.function.power);
+            var num = Factor.getNumerical(power)
+            var factor = new Factor(flattenedCopy.partOrDefault(Expression.function.base), num);
+            return [factor];
+        },
+        /**
+         * Gets a numerical value or string or expression.
+         * @param {Object} obj
+         * @return {Object/Number/String}
+         **/
+        getNumerical: function (obj) {
+            var result;
+            if (typeof obj === 'object') {
+                if (obj.type === Expression.type.variable) {
+                    result = obj.partOrDefault(Expression.type.variable);
+                    result = isNaN(result) ? result : parseFloat(result);
+                }
+            }
+            else if (typeof obj === 'number') {
+                result = obj;
+            }
+            else if (typeof obj === 'string') {
+                result = parseFloat(obj);
+            }
+            return result;
+        }
+    },
+    initialize: function (exp, count) {
+        var me = this;
+        me.exp = exp;
+        me.count = count;
+    },
+    properties: {
+        exp: null,
+        count: null
+    }
+});
